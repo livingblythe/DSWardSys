c8d928faa7e655cb8fee0c3a3d7472187ed9719a
livingblythe <josephwillis741@gmail.com>
2025-10-06 21:25:55 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let servers = [];
+
+app.post('/addserver', (req, res) => {
+  servers.push(req.body);
+  res.send({ status: 'added' });
+});
+
+app.post('/updateserver', (req, res) => {
+  const index = servers.findIndex(s => s.JobId === req.body.JobId);
+  if (index !== -1) servers[index] = req.body;
+  res.send({ status: 'updated' });
+});
+
+app.get('/getlist', (req, res) => {
+  res.send({ servers });
+});
+
+app.post('/removeserver', (req, res) => {
+  servers = servers.filter(s => s.JobId !== req.body.id);
+  res.send({ status: 'removed' });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server running');
+});
